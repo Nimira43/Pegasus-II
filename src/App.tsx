b2cd1e1e82ff49475cc81c21d79912483bad2942
Nimira43 <n.rabone72@aol.com>
2025-09-13 13:02:25 +0100

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return ( 
     <div>
       <h1 className='logo'>Pegasus II</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatem aperiam tempora quidem corrupti aliquid.</p>
     </div>
   )
 }
