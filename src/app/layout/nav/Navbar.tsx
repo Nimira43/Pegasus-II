a185f55fa310424312c3086709e0cacab9e30cb2
Nimira43 <n.rabone72@aol.com>
2025-09-13 19:40:27 +0100

    nav

--- src/app/layout/nav/Navbar.tsx
+++ src/app/layout/nav/Navbar.tsx
@@ -3,8 +3,13 @@ export default function Navbar() {
     <header className='p-3 w-full fixed top-0 z-50 bg-main'>
       <div className='flex align-middle items-center px-10 mx-auto gap-6'>
         <a className='max-h-16 text-light flex items-center gap-3 pr-6'>
-          <h3 className='text-2xl font-medium text-light logo'>Pegasus</h3>
+          <h3 className='text-2xl text-light logo'>Pegasus II</h3>
         </a>
+        <nav className='flex gap-3 my-2 uppercase text-lg text-white'>
+          <a>Events</a>
+          <a>Create</a>
+        </nav>
+        
       </div>
     </header>
   )
