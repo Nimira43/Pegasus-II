26cfd6cb114326013401226a5a33da14911622d6
Nimira43 <n.rabone72@aol.com>
2025-09-13 21:54:08 +0100

    Bringing in user image to card

--- src/features/events/dashboard/EventCard.tsx
+++ src/features/events/dashboard/EventCard.tsx
@@ -1,8 +1,22 @@
+import user from '../../../images/user.png'
+
 export default function EventCard() {
   return (
     <div className='card card-border bg-grey-light-extra w-full'>
       <div className='card-body'>
-        <h2 className='card-title'>Title</h2>
+        <div className='flex gap-3 items-center'>
+          <figure className='card-figure w-14 rounded-lg'>
+            <img 
+              src={user}
+              alt='User'  
+            />
+          </figure>
+          <div>
+            <h2 className='card-title'>Event Title</h2>
+            <p className='text-sm text-grey-dark'>Hosted by John</p>  
+          </div>
+        </div>
+        
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, porro!</p>
         <div className='card-actions justify-end'>
           <button className='btn bg-main text-light hover:bg-main-dark btn-hover'>Click</button>
