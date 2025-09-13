b85c4ff8345188933c5ed3a3566dd30b01896fd0
Nimira43 <n.rabone72@aol.com>
2025-09-13 21:44:01 +0100

    Images brought in

--- src/app/layout/nav/Navbar.tsx
+++ src/app/layout/nav/Navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
           <h3 className='text-2xl text-light logo'>Pegasus II</h3>
         </a>
         <nav className='flex gap-3 my-2 uppercase text-lg text-white'>
-          <a>Events</a>
-          <a>Create</a>
+          <a className='font-normal'>Events</a>
+          <a className='font-normal'>Create</a>
         </nav>
         <div className='flex align-middle ml-auto gap-3'>
           <button className='btn bg-light text-main hover:bg-grey-light btn-hover'>Login</button>
