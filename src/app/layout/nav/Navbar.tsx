f50175b7cf1db1b42cb026273e57b0ced75c2624
Nimira43 <n.rabone72@aol.com>
2025-09-13 19:38:03 +0100

    Setting up header

--- src/app/layout/nav/Navbar.tsx
+++ src/app/layout/nav/Navbar.tsx
@@ -1,7 +1,11 @@
 export default function Navbar() {
   return (
-    <header>
-      Header
+    <header className='p-3 w-full fixed top-0 z-50 bg-main'>
+      <div className='flex align-middle items-center px-10 mx-auto gap-6'>
+        <a className='max-h-16 text-light flex items-center gap-3 pr-6'>
+          <h3 className='text-2xl font-medium text-light logo'>Pegasus</h3>
+        </a>
+      </div>
     </header>
   )
 }
