caf4d585a20d46e37f48654e268b12e6c4293b8a
Nimira43 <n.rabone72@aol.com>
2025-09-13 22:10:10 +0100

    Working on EventAttendees avatar array

--- src/features/events/dashboard/EventAttendees.tsx
+++ src/features/events/dashboard/EventAttendees.tsx
@@ -1,7 +1,17 @@
 export default function EventAttendees() {
   return (
     <div className='avatar-group'>
-
+      {Array
+        .from({ length: 5 })
+        .map((_, index) => (
+          <div 
+            key={index}
+            className='avatar'
+          >
+            
+          </div>
+        ))
+      }
     </div>
   )
 }
