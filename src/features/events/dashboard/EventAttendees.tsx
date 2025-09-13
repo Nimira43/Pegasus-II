7d3e648de738512fe6ac21ff944e3d3bd709265a
Nimira43 <n.rabone72@aol.com>
2025-09-13 20:08:58 +0100

    Created EventCard and EventAttendees in dashboard folder

---
+++ src/features/events/dashboard/EventAttendees.tsx
@@ -0,0 +1,5 @@
+export default function EventAttendees() {
+  return (
+    <div>EventAttendees</div>
+  )
+}
---
+++ src/features/events/dashboard/EventCard.tsx
@@ -0,0 +1,5 @@
+export default function EventCard() {
+  return (
+    <div>EventCard</div>
+  )
+}
