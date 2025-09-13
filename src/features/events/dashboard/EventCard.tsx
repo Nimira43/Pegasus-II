0854b0c8a2c3bac6326c8fa16cd8181d459356b0
Nimira43 <n.rabone72@aol.com>
2025-09-13 20:29:57 +0100

    Added card-body div to card

--- src/features/events/dashboard/EventCard.tsx
+++ src/features/events/dashboard/EventCard.tsx
@@ -1,10 +1,12 @@
 export default function EventCard() {
   return (
     <div className='card card-border bg-grey-light-extra w-96'>
-      <h2 className='card-title'>Title</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, porro!</p>
-      <div className='card-actions justify-end'>
-        <button className='btn bg-main text-light hover:bg-main-dark btn-hover'>Click</button>
+      <div className='card-body'>
+        <h2 className='card-title'>Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, porro!</p>
+        <div className='card-actions justify-end'>
+          <button className='btn bg-main text-light hover:bg-main-dark btn-hover'>Click</button>
+        </div>
       </div>
     </div>
   )
