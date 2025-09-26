2ceb79add5689c68e57cc16e9a1721d51b441cfc
Nimira43 <n.rabone72@aol.com>
2025-09-26 12:58:25 +0100

    AppEvent type

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -4,3 +4,18 @@ export type AppUser = {
   photoURL?: string,
   email: string,
 }
+
+export type AppEvent = {
+  id: string,
+  title: string,
+  date: string,
+  description: string,
+  category: string,
+  city: string,
+  venue: string,
+  latitude: number,
+  longitude: number,
+  hostUid: string,
+  attendees: AppUser[],
+  attendeeIds: string[],
+}
