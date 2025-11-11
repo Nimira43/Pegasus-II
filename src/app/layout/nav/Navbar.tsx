26a30bcbcd7da9aac19d7b57453fdec769e2a400
Nimira43 <n.rabone72@aol.com>
2025-11-11 21:40:37 +0000

    Small styling chages made to Navbar

--- src/app/layout/nav/Navbar.tsx
+++ src/app/layout/nav/Navbar.tsx
@@ -7,18 +7,18 @@ export default function Navbar({
 }: Props) {
   return (
     <header className='p-3 w-full fixed top-0 z-50 bg-main'>
-      <div className='flex align-middle items-center px-10 mx-auto gap-6'>
+      <div className='flex align-middle items-center px-10 mx-auto gap-6 cursor-pointer'>
         <a className='max-h-16 text-light flex items-center gap-3 pr-6'>
           <h3 className='text-2xl text-light logo'>Pegasus II</h3>
         </a>
         <nav className='flex gap-3 my-2 uppercase text-lg text-white'>
           <a className='font-normal'>Events</a>
-          <button 
+          <a
             className='font-normal'
             onClick={() => setFormOpen(true)}
           >
             Create
-          </button>
+          </a>
         </nav>
         <div className='flex align-middle ml-auto gap-3'>
           <button className='btn bg-light text-main hover:bg-grey-light btn-hover'>Login</button>
