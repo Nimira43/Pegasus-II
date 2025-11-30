43eec4f7324978905e19a461bbcff26fc021e895
Nimira43 <n.rabone72@aol.com>
2025-11-30 10:21:23 +0000

    Updated motion.div properites.

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -55,9 +55,9 @@ export default function EventDashboard({
         <AnimatePresence>
           {formOpen && (
             <motion.div
-              initial={{ opacity: 0, x: -200}}
+              initial={{ opacity: 0, x: 200}}
               animate={{ opacity: 1, x: 0}}
-              exit={{ opacity: 0, x: -200 }}
+              exit={{ opacity: 0, x: 200 }}
               transition={{
                 duration: 0.3,
                 type: 'tween',
