faef8038cdfd78fb55d146c7da8a230c8d979505
Nimira43 <n.rabone72@aol.com>
2025-12-15 01:29:13 +0000

    Creating onSubmit function in EventForm

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -5,6 +5,10 @@ type Props = {
 export default function EventForm({
   setFormOpen
 }: Props) {
+  const onSubmit = () => {
+    
+  }
+
   return (
     <div className='card bg-grey-light-extra p-4 flex flex-col gap-3 w-full'>
       <h3 className='text-2xl font-medium text-center text-dark'>Create New Event</h3>
