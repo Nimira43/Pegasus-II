ebce1b7f2e0a58131dd3c885943e26875d424fee
Nimira43 <n.rabone72@aol.com>
2025-12-31 23:31:39 +0000

    new nav-btn and nav-link classes.

--- src/app/layout/nav/Navbar.tsx
+++ src/app/layout/nav/Navbar.tsx
@@ -12,17 +12,19 @@ export default function Navbar({
           <h3 className='text-2xl text-light logo'>Pegasus II</h3>
         </a>
         <nav className='flex gap-3 my-2 uppercase text-lg text-light '>
-          <a className='font-normal hover:text-grey-3 transitioning'>Events</a>
+          <a className='nav-link'>
+            Events
+          </a>
           <a
-            className='font-normal hover:text-grey-3 transitioning'
+            className='nav-link'
             onClick={() => setFormOpen(true)}
           >
             Create
           </a>
         </nav>
         <div className='flex align-middle ml-auto gap-3'>
-          <button className='btn bg-light text-main hover:bg-grey-3 transitioning'>Login</button>
-          <button className='btn bg-light text-main hover:bg-grey-3 transitioning'>Register</button>
+          <button className='btn nav-btn'>Login</button>
+          <button className='btn nav-btn'>Register</button>
         </div>
       </div>
     </header>
