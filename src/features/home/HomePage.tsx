525057d19ae6980cc87f24b5dda9cdd528f34df0
Nimira43 <n.rabone72@aol.com>
2026-03-31 19:23:27 +0100

    Created HomePage content.

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,5 +1,23 @@
+import { GiPegasus } from 'react-icons/gi'
+import { Link } from 'react-router'
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='hero'>
+      <div className='hero-content text-center'>
+        <div className='max-w-2xl flex flex-col items-center'>
+          <GiPegasus className='size-96 text-main' />
+          <h1 className='text-5xl py-6 logo text-main font-bold'>
+            Pegasus II
+          </h1>
+          <Link 
+            className='btn btn-lg event-btn'
+            to='/events'
+          >
+            View Events
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
