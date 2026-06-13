e273766acdee5975ea3f69d56fb683aede42b8b2
Nimira43 <n.rabone72@aol.com>
2026-06-13 15:21:10 +0100

    fireBaseSeed.ts created.

---
+++ src/lib/firebase/firebaseSeed.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from 'fs'
+import { initializeApp, cert } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+import { getAuth } from 'firebase-admin/auth'
+
+const serviceAccount = JSON
+  .parse(
+    readFileSync(
+      './src/lib/firebase/admin-creds.json', 'utf8'
+    )
+  )
+
+initializeApp({
+  credential: cert(serviceAccount)
+})
+
+const db = getFirestore()
+const auth = getAuth()
+
