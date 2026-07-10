639eae3be21251b5f8995d6d232f2bbca9450c0c
Nimira43 <n.rabone72@aol.com>
2026-07-10 01:56:36 +0100

    useCollection hook.

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,45 +1,23 @@
 import EventCard from './EventCard'
 import Counter from '../../counter/Counter'
-import { useAppDispatch, useAppSelector } from '../../../lib/stores/store'
-import { useCallback, useSyncExternalStore } from 'react'
-import { collection, onSnapshot, query } from 'firebase/firestore'
-import { db } from '../../../lib/firebase/firebase'
-import type { FirestoreAppEvent } from '../../../lib/types'
-import { setEvents } from '../eventSlice'
+import type { AppEvent } from '../../../lib/types'
+import { useCollection } from '../../../lib/hooks/useCollection'
 
 export default function EventDashboard() {
-  const dispatch = useAppDispatch()
-  const { events: appEvents } = useAppSelector(state => state.event)
-
-  const listenToEvents = useCallback(() => {
-    const q = query(collection(db, 'events'))
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      const events: FirestoreAppEvent[] = []
-      snapshot.forEach((doc) => {
-        events.push({ id: doc.id, ...doc.data() } as FirestoreAppEvent)
-      })
-      dispatch(setEvents(events))
-    })
-
-    return () => {
-      unsubscribe()
-    }
-  }, [dispatch])
-
-  useSyncExternalStore(listenToEvents, () => appEvents)
+  const { data: appEvents, loading} = useCollection<AppEvent>({ path: 'events'})
+  
+  if (loading) return <div>Loading...</div>
 
   return (
     <div className='flex flex-row w-full gap-6'>
       <div className='w-3/5'>
         <div className='flex flex-col gap-4'>
-          {appEvents
-            .map((event) => (
-              <EventCard 
-                key={event.id}
-                event={event}
-              />
-            ))
-          }    
+          {appEvents?.map((event) => (
+            <EventCard 
+              key={event.id}
+              event={event}
+            />
+          ))}    
         </div>
       </div>
       <div className='w-2/5 overflow-hidden'>
