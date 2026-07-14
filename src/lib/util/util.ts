82cca3321ca6ff0f097b3e0a8ec1cad94bf231ee
Nimira43 <n.rabone72@aol.com>
2026-07-14 14:47:11 +0100

    ConvertTimeStamps function

--- src/lib/util/util.ts
+++ src/lib/util/util.ts
@@ -1,14 +1,32 @@
+import { Timestamp } from 'firebase/firestore'
+
 export function debounce<T extends unknown[], U>(
   callback: (...args: T) => PromiseLike<U> | U,
-  wait: number
+  wait: number,
 ) {
-  let timer: number
+  let timer: ReturnType<typeof setTimeout>
 
   return (...args: T): Promise<U> => {
     clearTimeout(timer)
-    
+
     return new Promise((resolve) => {
       timer = setTimeout(() => resolve(callback(...args)), wait)
     })
   }
+}
+
+export const convertTimestamps = (data: unknown): unknown => {
+  if (Array.isArray(data)) {
+    return data.map(convertTimestamps)
+  } else if (data instanceof Timestamp) {
+    return data.toDate().toISOString()
+  } else if (data && typeof data === 'object') {
+    return Object.fromEntries(
+      Object.entries(data).map(([key, value]) => [
+        key,
+        convertTimestamps(value),
+      ]),
+    )
+  }
+  return data
 }
